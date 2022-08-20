5c81cecf0c4756ef66e9b365995631148c32d81c
Carolina Daza Gomez <carolina.daza@pragma.com.co>
2022-08-20 03:41:48 -0500

    Realizar peticiones HTTP con el modulo propio de Angular llamado HttpClientModule

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http'; //Me ofrece un  monton de servicios
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -6,18 +7,24 @@ import { Injectable } from '@angular/core';
 export class GifsService {
   private _historial: string[] = []
 
+  private api: any = "01BRnwJAE6xN4PVNJ0OxHMyvoQhFMabe"
+
   get historial() {    
     return [...this._historial]
   }
+  constructor(private http: HttpClient){}
 
   buscarGifs(query: string = "") {
     query = query.trim().toLocaleLowerCase();
-    this._historial.unshift(query);
-    
+
     if (!this._historial.includes(query)) {
+      this._historial.unshift(query);
       this._historial = this._historial.splice(0,10);      
     }
-
-    console.log(this._historial);
+    
+    this.http.get("https://api.giphy.com/v1/gifs/search?api_key=01BRnwJAE6xN4PVNJ0OxHMyvoQhFMabe&q=dragon ball z&limit=10")
+      .subscribe((resp: any) => {
+        console.log("resp",resp.data)
+      })
   }
 }
