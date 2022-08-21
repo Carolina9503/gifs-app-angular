59695f7659c402117baff1bd780d8d5209b762a9
Carolina Daza Gomez <carolina.daza@pragma.com.co>
2022-08-21 17:02:36 -0500

    guardar en local storage y recordar que la diferencia con el session storage es que este borra la data al cerrar y abrir el navegador

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -14,14 +14,21 @@ export class GifsService {
   get historial() {    
     return [...this._historial]
   }
-  constructor(private http: HttpClient){}
+  constructor(private http: HttpClient){
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || []
+    // if (localStorage.getItem("historial")) {
+    //   this._historial = JSON.parse(localStorage.getItem('historial')!)       
+    // }
+  }
 
   buscarGifs(query: string = "") {
     query = query.trim().toLocaleLowerCase();
 
     if (!this._historial.includes(query)) {
       this._historial.unshift(query);
-      this._historial = this._historial.splice(0,10);      
+      this._historial = this._historial.splice(0,10);   
+      
+      localStorage.setItem("historial", JSON.stringify(this._historial))
     }
     
     this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=01BRnwJAE6xN4PVNJ0OxHMyvoQhFMabe&q=${ query }&limit=10`)
